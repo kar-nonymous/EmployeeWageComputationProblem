6b85f38a82dcc8487ee48d8064ef449baa47d08d
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-04 12:22:14 +0530

    UC13

---
+++ UC13/CompanyWageDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC13
+{
+    class CompanyWageDetails
+    {
+        public string company;
+        public int empRatePerHour, numOfWorkingDays, maxWorkingHours;
+        public int totalEmpWage;
+        public List<int> dailyWageList;
+
+        public CompanyWageDetails(string company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHours)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxWorkingHours = maxWorkingHours;
+            this.totalEmpWage = 0;
+            dailyWageList = new List<int>();
+        }
+
+        public void GetDailyWageDetails()
+        {
+            for (int currentDay = 0; currentDay < dailyWageList.Count; currentDay++)
+            {
+                Console.WriteLine("Day " + (currentDay + 1) + " Employee Hours " + dailyWageList[currentDay]);
+            }
+
+        }
+        public void GetTotalWageDetails()
+        {
+            
+            Console.WriteLine("Total Wage for " + company + " Employee is: " + totalEmpWage);
+            
+        }
+    }
+}
---
+++ UC13/EmpWageBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC13
+{
+    class EmpWageBuilderObject : IEmpWageBuilderObject
+    {
+
+        private List<CompanyWageDetails> CompanyWageDetailsList;
+
+        public EmpWageBuilderObject()
+        {
+            CompanyWageDetailsList = new List<CompanyWageDetails>();
+        }
+
+        public void AddCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHours)
+        {
+            CompanyWageDetails cwdObj = new CompanyWageDetails(company, empRatePerHour, numOfWorkingDays, maxWorkingHours);
+            CompanyWageDetailsList.Add(cwdObj);
+        }
+
+        public void ComputeEmpWage()
+        {
+            foreach (CompanyWageDetails cwdObj in CompanyWageDetailsList)
+            {
+                cwdObj.totalEmpWage = CalculateTotalEmployeeWage(cwdObj);
+                cwdObj.GetDailyWageDetails();
+                cwdObj.GetTotalWageDetails();
+            }
+        }
+
+        private int CalculateTotalEmployeeWage(CompanyWageDetails cwdObj)
+        {
+            const int IS_ABSENT = 0;
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+
+            
+            Console.WriteLine("Wage Calculation for " + cwdObj.company + " Employee");
+            
+            Random randObj = new Random();
+            int empHours = 0, totalWorkingHoursCumulative = 0;
+
+            for (int currentDay = 1; currentDay <= cwdObj.numOfWorkingDays && totalWorkingHoursCumulative <= cwdObj.maxWorkingHours; currentDay++)
+            {
+                int option = randObj.Next(0, 3);
+                switch (option)
+                {
+                    case IS_ABSENT:
+                        empHours = 0;
+                        break;
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHours = 8;
+                        break;
+                    default:
+                        break;
+                }
+                if (totalWorkingHoursCumulative + empHours > cwdObj.maxWorkingHours)
+                    empHours = 0;
+                totalWorkingHoursCumulative += empHours;
+                cwdObj.dailyWageList.Add(empHours);
+            }
+            Console.WriteLine("Total Employee Hours " + totalWorkingHoursCumulative);
+            return totalWorkingHoursCumulative * cwdObj.empRatePerHour;
+        }
+
+    }
+}
