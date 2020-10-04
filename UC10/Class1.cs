76f43f4168833bf337fb78cd48b2702ec7f4718e
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-04 12:19:41 +0530

    UC10

---
+++ UC10/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC10
+{
+    class CompanyWageDetails
+    {
+        public string company;
+        public int empRatePerHour, numOfWorkingDays, maxWorkingHours;
+        public int totalEmpWage;
+
+        public CompanyWageDetails(string company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHours)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxWorkingHours = maxWorkingHours;
+            this.totalEmpWage = 0;
+        }
+
+        public void GetDetails()
+        {
+            
+            Console.WriteLine("Total Employee Wage for " + company + " Employee is: " + totalEmpWage);
+            
+        }
+    }
+}
---
+++ UC10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            EmpWageBuilderObject ewbObj = new EmpWageBuilderObject();
+            ewbObj.AddCompanyEmpWage("Flipkart", 200, 20, 100);
+            ewbObj.AddCompanyEmpWage("Amazon", 250, 20, 70);
+            ewbObj.ComputeEmpWage();
+        }
+
+    }
+}
