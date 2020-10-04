1b0aa1a63cb64884081ffc55c6d3120323ba2a2a
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-04 12:06:23 +0530

    UC2

---
+++ UC2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_FULL_TIME = 1;
+            const int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck==IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp wage: " + empWage);
+        }
+    }
+}
